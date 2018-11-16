a4a0c145e99578bc41e4a57559a573dba90461db
munirchittagong@yahoo.com <munirchittagong@yahoo.com>
2018-11-16 22:35:04 +0000

    40. updating the events

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -3,12 +3,12 @@ import EventListItem from './EventListItem.jsx'
 
 class EventList extends Component {
 	render() {
-		const {events, onEventEdit} = this.props;
+		const {events, onEventOpen} = this.props;
 		return (
 			<div>
 				<h1> Event List </h1>
 				{events.map((event) => (
-					<EventListItem key={event} event={event} onEventEdit={onEventEdit} />
+					<EventListItem key={event} event={event} onEventOpen={onEventOpen} />
 				))}
 			</div>
 		)
