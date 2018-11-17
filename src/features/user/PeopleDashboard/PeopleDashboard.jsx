211b43b1534005c80af786764d2c0ebe36f6cdb6
munirchittagong@yahoo.com <munirchittagong@yahoo.com>
2018-11-17 12:04:19 +0000

    44. stateless functional components

---
+++ src/features/user/PeopleDashboard/PeopleDashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PeopleDashboardPage = () => {
+	return (
+		<div>
+			<h1> People Dashboard Page</h1>
+		</div>		
+	)
+}
+
+export default PeopleDashboardPage
+
+
---
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SettingsDashboardPage = () => {
+	return (
+		<div>
+			<h1>Settings Dashboard Page</h1>
+		</div>		
+	)
+}
+
+export default SettingsDashboardPage
