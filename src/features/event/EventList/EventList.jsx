62b8e64213fe8018fc2fa8cfa63a9091d7c978ae
munirchittagong@yahoo.com <munirchittagong@yahoo.com>
2018-11-24 14:57:56 +0000

    73. connecting the event actions to the event form

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -3,12 +3,11 @@ import EventListItem from './EventListItem.jsx'
 
 class EventList extends Component {
 	render() {
-		const {events, onEventOpen, deleteEvent} = this.props;
+		const {events, deleteEvent} = this.props;
 		return (
 			<div>
-				<h1> Event List </h1>
 				{events.map((event) => (
-					<EventListItem key={event} event={event} onEventOpen={onEventOpen} deleteEvent={deleteEvent} />
+					<EventListItem key={event} event={event} deleteEvent={deleteEvent} />
 				))}
 			</div>
 		)
